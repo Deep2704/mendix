9d2c17856e00b7eecdcf1d63f0407565776789b0
Deep2704 <deepprabhu1995@gmail.com>
2023-06-09 17:45:55 +0530

    last commit i think

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ cd ~/docker-mendix-buildpack/downloads ~/docker-mendix-buildpack/build
 cd ~/docker-mendix-buildpack
 mkdir -p downloads build
 cd ~
+chmod 774 mendix/App_6.0.0.252a3755.mda
+chmod 774 mendix/script.sh
 cd mendix
 mv App_6.0.0.252a3755.mda application.mpk
 cp application.mpk ~/docker-mendix-buildpack/downloads
